8de11d7aeb96a5e5f08439f473df65790374ea91
Sebastian Höffner <info@sebastian-hoeffner.de>
2024-07-19 01:36:01 +0200

    Fix mypy Issues in api and conf.py
    Also add types-jsonschema as a lint dependency.

--- pyDataverse/docs/source/conf.py
+++ pyDataverse/docs/source/conf.py
@@ -142,7 +142,7 @@ htmlhelp_basename = "pyDataverse"
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
+latex_elements: dict[str, str] = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
